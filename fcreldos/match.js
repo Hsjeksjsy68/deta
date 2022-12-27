b8bc6217c4d541b096b7dba1881ea6de2cd3c8c7
Abdur Rakib <89575718+Hsjeksjsy68@users.noreply.github.com>
2022-12-27 00:00:40 -1200

    Update match.js

--- fcreldos/match.js
+++ fcreldos/match.js
@@ -1,4 +1,4 @@
 document.getElementById('line-1-section-1').innerHTML = ('FC RELDOS');
 document.getElementById('line-1-section-2').innerHTML = (' INTER MILAN');
 document.getElementById('line-2-section-1').innerHTML = ('FC RELDOS');
-document.getElementById('line-2-section-2').innerHTML = (' PSG');
+document.getElementById('line-2-section-2').innerHTML = (' PARIS SAINT-GERMAIN');
